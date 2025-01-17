2dce05f3520f70e642c2e944596eb4e4d2c91ada
Kavibharathi17 <kavibharathi.rv2011@gmail.com>
2025-01-17 12:58:33 +0530

    project

---
+++ script.js
@@ -0,0 +1,27 @@
+function showcontent(id) {
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(function (element) {
+        element.classList.remove('active');
+    });
+    var addcontent = document.getElementById(id);
+    if (addcontent) {
+        addcontent.classList.add('active');
+    }
+}
+
+var index=0;
+showslide();
+function showslide(){
+    var i;
+    var x=document.querySelectorAll('.slide');
+    x.forEach((item) => {
+        item.style.display = "none";
+    });    
+    index++;
+    if(index>x.length){
+        index=1;
+    }
+    x[index-1].style.display='block';
+    setTimeout(showslide,2000);
+}
+
